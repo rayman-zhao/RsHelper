ee9220ce76dfcf783a81365c82e7a7796b88992b
Yu Zhao <zhao.y@163.com>
2025-09-05 13:30:51 +0800

    Build on macOS

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RsHelper",
+    platforms: [
+    	.macOS(.v15),
+    ],
     products: [
         .library(
             name: "RsHelper",
